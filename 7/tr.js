0477b17b40c182359d1db8541101a0c5ca7bb6d7
Elena_Kusochkova <83718500+gnil000@users.noreply.github.com>
2021-11-12 12:31:41 +0300

    Add files via upload

---
+++ 7/tr.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        arrows : false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [{
+            breakpoint: 500,
+            settings:{
+                arrows: true,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+            }
+        }],
+        mobileFirst: true,
+    }); 
+});
+
+
+
